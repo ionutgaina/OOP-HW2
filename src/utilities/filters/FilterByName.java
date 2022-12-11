fae6b82dc1f9551172af8d261d60daf6eed488a8
IonutGaina <gainaionutz@gmail.com>
2022-12-11 14:29:48 +0200

    fix filter bug

--- src/utilities/filters/FilterByName.java
+++ src/utilities/filters/FilterByName.java
@@ -8,6 +8,10 @@ import java.util.stream.Collectors;
 public class FilterByName implements IFilter {
     @Override
     public ArrayList<Movie> doFilter(ArrayList<Movie> movies, ArrayList<String> filters) {
+        if(filters == null) {
+            return movies;
+        }
+
         return (ArrayList<Movie>) movies
                 .stream()
                 .filter(movie -> filters
